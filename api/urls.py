39f9a03e841aea884c86feec57593aaaa7afd983
Diego Velasquez <velasquezdiego2404@gmail.com>
2026-05-24 03:06:13 -0500

    version FINA

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,18 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('pacientes', views.PacienteViewSet)
-router.register('metricas', views.MetricaViewSet)
-router.register('alertas', views.AlertaViewSet)
-router.register('notas', views.NotaClinicaViewSet)
+router.register(r'pacientes', views.PacienteViewSet)
+router.register(r'metricas', views.MetricaViewSet)
+router.register(r'alertas', views.AlertaViewSet)
+router.register(r'notas', views.NotaClinicaViewSet)
 router.register(r'planes', views.PlanCuidadoViewSet, basename='planes')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/login/', views.login_token, name='api_login'),
+    path('mis-metricas/', views.mis_metricas, name='mis_metricas'),
+    path('registrar-metrica/', views.registrar_metrica_movil, name='registrar_metrica_movil'),
+    path('mi-plan/', views.mi_plan_cuidados, name='mi_plan'),
+]
